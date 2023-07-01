25975e3e7b3278c2d39d9b77b154f8308efe08bc
alimeshkini99 <137733251+alimeshkini99@users.noreply.github.com>
2023-07-01 16:19:46 +0330

    nothing_important

--- model/book_model.js
+++ model/book_model.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bookModel = new mongoose.Schema({
   title: { type: String, required:'{PATH} is required ',lowercase: true, },
   isbn: { type: Number,required:true,unique:true},
+  About:{type:String},
   author: {
     type: mongoose.Schema.Types.ObjectId, 
     ref:'author'
